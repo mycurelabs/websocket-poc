803122062bc9d388b66466e9fd420cdbafd2b532
John Leonard Tubig <tubig.jlu@gmail.com>
2020-07-06 14:52:41 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const yargs = require('yargs');
+const bunyan = require('bunyan');
+const WebSocket = require('ws');
+
+function createLogger (opts) {
+  return bunyan.createLogger({ 
+    name: opts.name, 
+    serializers: bunyan.stdSerializers,
+    streams: [
+      opts.logsDir && {
+        level: 'debug',
+        name: ['logfile', 'debug'].join('-'),
+        type: 'rotating-file',
+        path: path.join(opts.logsDir, [opts.name, 'debug', 'log'].join('.')),
+      },
+      {
+        name: 'stdout',
+        level: 'trace',
+        type: 'stream',
+        stream: process.stdout,
+      },
+    ].filter(Boolean), 
+  });
+}
+
+async function runServer (opts) {
+  const log = createLogger({ ...opts, name: 'server' });
+  log.debug({ opts }, 'setting up server...');
+  const wss = new WebSocket.Server({ port: opts.port });
+  wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+      log.debug({ message: JSON.parse(message, null, 2) }, 'received message');
+      // echo message
+      ws.send(message);
+    });
+    ws.send(JSON.stringify({
+      type: 'message',
+      payload: `Welcome!`
+    }));
+  });
+  wss.on('listening', () => log.info(`server listening @ port ${opts.port}`));
+}
+
+async function runClient (opts) {
+  const log = createLogger({ ...opts, name: 'client' });
+  log.debug({ opts }, 'connecting to server...');
+  const ws = new WebSocket(opts.url);
+  ws.on('message', (message) => {
+    log.debug({ message: JSON.parse(message, null, 2) }, 'received message');
+  });
+  ws.on('open', () => {
+    log.info(`connected to server ${opts.url}`)
+    ws.send(JSON.stringify({
+      type: 'message',
+      payload: 'connected! will send message every 5s',
+    }));
+    let counter = 0;
+    setInterval(() => {
+      ws.send(JSON.stringify({
+        type: 'message',
+        payload: `send message ${++counter}`,
+      }));
+    }, 5000);
+  });
+}
+
+// args parser
+yargs
+  .option('logs-dir', {
+    type: 'string',
+    description: 'logs directory',
+  })
+  .command(
+    'server',
+    'run websocket server',
+    yargs => yargs
+      .option('port', { 
+        type: 'number', 
+        default: 8080,
+        description: 'Port to bind the server to',
+      })
+      .option('path', { 
+        type: 'string', 
+        default: '/wss',
+        description: 'Path to listen to'
+      }),
+    runServer,
+  )
+  .command(
+    'client',
+    'run websocket client',
+    yargs => yargs
+      .option('url', { 
+        type: 'string', 
+        default: 'http://localhost:8080/wss',
+      }),
+    runClient,
+  )
+  .argv;
